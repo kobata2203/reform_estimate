69ffbd34cb486a552d5a6d1e7e7c2b574117bffe
kobata2203 <o78.kobata.takahiro@gmail.com>
2024-08-27 16:47:36 +0900

    update20240826_3

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,6 +65,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'department_name' => $data['department_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
