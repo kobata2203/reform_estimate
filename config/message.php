ec2ce209b7e46a3cb4cfc0260b50a00743a3f360
tanakast <sys_h.tanaka@servantop.co.jp>
2024-10-30 09:47:35 +0900

    表示メッセージの追加・修正

--- config/message.php
+++ config/message.php
@@ -1,5 +1,9 @@
 <?php
 return [
     'regist_complete' => env('REGIST_COMPLETE','登録完了しました。'),
-    'regist_fail' => env('REGIST_COMPLETE','登録エラーが発生しました。'),
+    'update_complete' => env('UPDATE_COMPLETE','更新完了しました。'),
+    'delete_complete' => env('DELETE_COMPLETE','削除完了しました。'),
+    'regist_fail' => env('REGIST_FAIL','登録処理に失敗しました。管理者にご連絡ください。'),
+    'update_fail' => env('UPDATE_FAIL','更新処理に失敗しました。管理者にご連絡ください。'),
+    'delete_fail' => env('DELETE_FAIL','削除処理に失敗しました。管理者にご連絡ください。'),
 ];
