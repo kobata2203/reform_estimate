2d2698266eaeca2ffcd47cf2bd2f3ae587a9d044
tanakast <sys_h.tanaka@servantop.co.jp>
2024-11-07 11:03:47 +0900

    営業者メニューのデザイン基本処理を修正
    テンプレートレイアウトおよび共通用のJS処理の作成。
    共通デザインの反映例として作成。

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- TODO:CSSから共通処理を専用ファイルにまとめる -->
+    <script src="{{ asset('/js/utill.js') }}"></script>
+    @yield('headder')
+
+</head>
+
+<body>
+@yield('content')
+</body>
+
+</html>
