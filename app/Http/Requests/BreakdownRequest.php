1597f88e4b290b2c1d3948fb6974302dc68b7615
tanakast <sys_h.tanaka@servantop.co.jp>
2024-12-02 09:28:22 +0900

    内訳明細処理の修正

--- app/Http/Requests/BreakdownRequest.php
+++ app/Http/Requests/BreakdownRequest.php
@@ -5,16 +5,19 @@ namespace App\Http\Requests;
 use App\Models\Breakdown;
 use Illuminate\Foundation\Http\FormRequest;
 use App\Models\ConstructionItem;
-use App\Rules\SpecificationRequired;
+use App\Rules\ConstructionItemRequired;
 
 class BreakdownRequest extends FormRequest
 {
-    protected $constructionItem;
+    protected $construction_item;
     protected $breakdown;
-    public function __construct()
+    public function __construct(
+        ConstructionItem $constructionItem,
+        Breakdown $breakdown
+    )
     {
-        $this->constructionItem = new ConstructionItem();
-        $this->breakdown = new Breakdown();
+        $this->construction_item = $constructionItem;
+        $this->breakdown = $breakdown;
     }
 
     /**
@@ -36,15 +39,16 @@ class BreakdownRequest extends FormRequest
     {
         // デフォルト外のバリデートは独自処理を作成して呼び出す
         return [
-            'estimate_id'         => 'required',
-            'construction_id'     => 'required',
-            'construction_item.*' => 'required',
-            'specification.*'     => [new SpecificationRequired($this->all()), 'max:30'],
-            'quantity.*'          => 'required|integer|min:1|max:20',
-            'unit.*'              => 'required|max:5',
-            'unit_price.*'        => 'required|integer|min:1|max:9999999',
-            'amount.*'            => 'required|integer|min:1',
-            'remarks2.*'          => 'max:30',
+            'estimate_id'   => 'required',
+            'item.*'        => 'required',
+            'maker.*'       => [new ConstructionItemRequired($this->all(), 'maker', $this->construction_item), 'max:30'],
+            'series_name.*' => [new ConstructionItemRequired($this->all(), 'series_name', $this->construction_item), 'max:30'],
+            'item_number.*' => [new ConstructionItemRequired($this->all(), 'item_number', $this->construction_item), 'max:30'],
+            'quantity.*'    => 'required|integer|min:1|max:20',
+            'unit.*'        => 'required|max:5',
+            'unit_price.*'  => 'required|integer|min:1|max:9999999',
+            'amount.*'      => 'required|integer|min:1',
+            'remarks.*'     => [new ConstructionItemRequired($this->all(), 'remarks', $this->construction_item), 'max:30'],
         ];
     }
 }
