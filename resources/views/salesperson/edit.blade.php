088610cb146368cf448ba0ad35995c51172e0fb8
kobata2203 <o78.kobata.takahiro@gmail.com>
2025-01-16 16:17:18 +0900

    部署名selectメニューの修正

--- resources/views/salesperson/edit.blade.php
+++ resources/views/salesperson/edit.blade.php
@@ -14,24 +14,28 @@
                 @method('PUT')
                 <div class="form-group">
                     <label for="name">氏名</label>
-                    <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}" required>
+                    <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $user->name) }}" required>
+                </div>
+                <div class="form-group">
+                    <label for="department_name">部署名</label>
+                    <select id="department_name" name="department_id"  class="department_id">
+                            @foreach($departments as $department)
+                                <option value={{ $department->id }}>{{ $department->name }}</option>
+                            @endforeach
+                    </select>
                 </div>
                 <div class="form-group">
                     <label for="email">メールアドレス</label>
-                    <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}" required>
+                    <input type="email" name="email" id="email" class="form-control" value="{{ old('email', $user->email) }}" required>
                 </div>
                 <div class="form-group">
                     <label for="password">パスワード</label>
                     <input type="password" name="password" id="password" class="form-control">
                 </div>
-                <div class="form-group">
-                    <label for="department_name">部署名</label>
-                    <input type="text" name="department_name" id="department_name" class="form-control" value="{{ $user->department_name }}" required>
-                </div>
                 <button type="submit" class="btn btn-primary margin-left-300">更新</button>
                 <button type="button"
                 onclick="window.location.href='{{ route('manager_menu') }}'"
-                class="btn btn-primary custom-border">管理者メニュー
+                class="btn btn-primary custom-border">戻る
 
                 </button>
             </form>
