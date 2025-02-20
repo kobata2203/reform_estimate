f88008fc48bcb6e05f7d7303e28b8420a4c72297
servantop-wmiyazato <servantop.w.miyazato@gmail.com>
2025-02-20 09:50:25 +0900

    Auto Logout

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use ;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -36,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class, 
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\AutoLogoutMiddleware::class,
         ],
 
         'api' => [
@@ -54,6 +56,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         // 'auth' => \App\Http\Middleware\Authenticate::class,
         // 'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auto-logout' => \App\Http\Middleware\AutoLogoutMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
