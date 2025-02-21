b9cb6581c270aafdeb837286fb7a8b47ea9595f3
W.Miyazato <servantop.w.miyazato@gmail.com>
2025-02-21 20:32:19 +0900

    AutoLogoutMiddleware delete no need code

--- app/Http/Middleware/AutoLogoutMiddleware.php
+++ app/Http/Middleware/AutoLogoutMiddleware.php
@@ -10,33 +10,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AutoLogoutMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        // if (Auth::check()) {
-        //     $user = Auth::user();
-        //     $lastActivityKey = 'last_activity_' . $user->role;
-
-        //     $lastActivity = session($lastActivityKey);
-
-        //     if ($lastActivity && Carbon::parse($lastActivity)->diffInMinutes(now()) >= 1) {
-        //         Auth::logout();
-
-        //         session()->flush();
-
-        //         if ($user->role === 'admin') {
-        //             return redirect('/admin/login')->with('message', 'セッションの有効期限が切れました。再度ログインしてください。');
-        //         }
-
-        //         return redirect('/sales/login')->with('message', 'セッションの有効期限が切れました。再度ログインしてください。');
-        //     }
-
-        //     session([$lastActivityKey => now()]);
-        // }
         if (Auth::guard('sales')->check()) {
             return $this->checkSessionTimeout($request, $next, 'sales', '/sales/login');
         }
