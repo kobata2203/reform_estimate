0dfcdee3f4059277be186f444416e3dd6d8c9f69
W.Miyazato <servantop.w.miyazato@gmail.com>
2025-02-27 17:36:55 +0900

    Add Login Title 営業者　管理者

--- resources/views/auth/sales/login.blade.php
+++ resources/views/auth/sales/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">営業者{{ __('Login') }}　</div>
 
                 <div class="card-body">
                     @if (session('logout_message'))
